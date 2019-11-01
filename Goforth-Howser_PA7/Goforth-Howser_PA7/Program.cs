7c95d3683a29d2e3a9c6cf4f4f838060e5d91de7
abalachi6 <47046304+abalachi6@users.noreply.github.com>
2019-10-31 22:27:59 -0500

    Add files via upload

---
+++ Goforth-Howser_PA7/Goforth-Howser_PA7/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goforth_Howser_PA7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            double buildTime = 0;
+
+            Console.WriteLine("\t\tGozer's Destroyers!\n");
+
+            List<Robot> robotsBuilt = new List<Robot>();
+
+            while (buildTime < 12)
+            {
+                counter++;
+
+                Console.Write("\nEnter Robot {0} ID: ", counter);
+                string idName = Console.ReadLine();
+
+                Console.Write("Enter number of batteries: ");
+                int batteries;
+                while (int.TryParse(Console.ReadLine(), out batteries) != true)
+                {
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("Invalid value. Try again: ");
+                    Console.ForegroundColor = color;
+                }
+
+                Console.Write("Enter number of components: ");
+                int components;
+                while (int.TryParse(Console.ReadLine(), out components) != true)
+                {
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("Invalid value. Try again: ");
+                    Console.ForegroundColor = color;
+                }
+
+                Console.Write("How many hours to buld Robot {0}: ", counter);
+                double hours;
+                while (double.TryParse(Console.ReadLine(), out hours) != true)
+                {
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("Invalid value. Try again: ");
+                    Console.ForegroundColor = color;
+                }
+                buildTime += hours;
+
+                robotsBuilt.Add(new Robot(idName, batteries, components, hours));
+            }
+
+            Console.Write("\nLow-power robot summary:\n");
+            foreach (Robot thing in robotsBuilt)
+            {
+                if (thing.IsHighPower() != true)
+                {
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    thing.PrintDetails();
+                    Console.ForegroundColor = color;
+                }
+            }
+
+            Console.Write("\nHigh-power robot summary:\n");
+            foreach (Robot thing in robotsBuilt)
+            {
+                if (thing.IsHighPower() == true)
+                {
+                    ConsoleColor color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    thing.PrintDetails();
+                    Console.ForegroundColor = color;
+                }
+            }
+
+            double cost = 0;
+            foreach (Robot thing in robotsBuilt)
+            {
+                cost += thing.GetCost();
+            }
+            Console.Write("\nTotal cost is {0:C}", cost);
+
+
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ Goforth-Howser_PA7/Goforth-Howser_PA7/Robot.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goforth_Howser_PA7
+{
+    class Robot
+    {
+        string idnumber;
+        int batteries;
+        int components;
+        double ttbuild;
+
+        public string IDnumber
+        {
+            get
+            {
+                return idnumber;
+            }
+            set
+            {
+                idnumber = value;
+            }
+        }
+
+        public int Batteries
+        {
+            get
+            {
+                return batteries;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    batteries = value;
+                }
+                else
+                {
+                    batteries = value*-1;
+                }
+            }
+        }
+
+        public int Components
+        {
+            get
+            {
+                return components;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    components = value;
+                }
+                else if(value < 0)
+                {
+                    components = value * -1;
+                }
+                else 
+                {
+                    components = 1;
+                }
+            }
+        }
+
+        public double TTBuild
+        {
+            get
+            {
+                return ttbuild;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    ttbuild = value;
+                }
+                else
+                {
+                    ttbuild = value*-1;
+                }
+            }
+        }
+
+        public Robot()
+        {
+            idnumber= "Stay Puft";
+            batteries= 1;
+            components=1;
+            ttbuild= 0.0;
+
+        }
+
+        public Robot(string idNumber, int nBatteries, int nComponents, double buildT)
+        {
+            IDnumber = idNumber;
+            Batteries = nBatteries;
+            Components = nComponents;
+            TTBuild = buildT;
+        }
+
+        public double GetPower()
+        {
+            double power = (144.0*(batteries*batteries))/components;
+            return power;
+        }
+
+        public bool IsHighPower()
+        {
+            if(GetPower()>= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetCost()
+        {
+            double cost = 1.21 * ((10 * components) + (75 * ttbuild));
+            return cost;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("# ID#:{0}  ({1:F}W)  {2:C}\n  >Batteries:  {3}\tComponents:  {4}", idnumber, GetPower(), GetCost(), batteries, components);
+            
+        }
+    }
+}
